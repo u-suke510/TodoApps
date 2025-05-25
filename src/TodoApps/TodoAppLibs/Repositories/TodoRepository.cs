a25c06b521ba0166adcf14505ef62212119015b2
勇介 後藤 <usuke510@outlook.jp>
2025-05-25 17:27:12 +0900

    ToDoのリポジトリクラスの追加

---
+++ src/TodoApps/TodoAppLibs/Repositories/TodoRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using TodoAppLibs.Entities;
+using TodoAppLibs.Resources;
+
+namespace TodoAppLibs.Repositories
+{
+    /// <summary>
+    /// ToDo情報のRepositoryインターフェース
+    /// </summary>
+    public interface ITodoRepository : IRepositoryBase
+    {
+        /// <summary>
+        /// ToDo情報をToDoIDから取得します。
+        /// </summary>
+        /// <param name="id">ToDoID</param>
+        /// <returns>ToDo情報エンティティ</returns>
+        TTodo GetItemById(int id);
+    }
+
+    /// <summary>
+    /// ToDo情報のRepositoryクラス
+    /// </summary>
+    public class TodoRepository : RepositoryBase, ITodoRepository
+    {
+        /// <summary>
+        /// コンストラクタ。
+        /// </summary>
+        /// <param name="context">DBコンテキスト</param>
+        /// <param name="logger">ロガー</param>
+        public TodoRepository(AppDbContext context, ILogger<TodoRepository> logger) : base(context, logger)
+        {
+        }
+
+        /// <summary>
+        /// ToDo情報をToDoIDから取得します。
+        /// </summary>
+        /// <param name="id">ToDoID</param>
+        /// <returns>ToDo情報エンティティ</returns>
+        public TTodo GetItemById(int id)
+        {
+            var item = context.ToDos.SingleOrDefault(x => x.Id == id && !x.DelFlg);
+            logger.LogInformation(MsgResource.FmtDbSelectCount, item == null ? 0 : 1);
+            return item;
+        }
+    }
+}
---
+++ src/TodoApps/TodoAppLibs/Utils/RepositoryBase.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace TodoAppLibs
+{
+    /// <summary>
+    /// Repositoryクラスのベースインタフェース
+    /// </summary>
+    public interface IRepositoryBase
+    {
+        /// <summary>
+        /// エンティティを登録します。
+        /// </summary>
+        /// <param name="entity">登録エンティティ</param>
+        void InsEntity(EntityBase entity);
+
+        /// <summary>
+        /// エンティティを更新します。
+        /// </summary>
+        /// <param name="entity">更新エンティティ</param>
+        void UpdEntity(EntityBase entity);
+    }
+
+    /// <summary>
+    /// Repositoryクラスのベースクラス
+    /// </summary>
+    public abstract class RepositoryBase : IRepositoryBase
+    {
+        /// <summary>
+        /// DBコンテキスト
+        /// </summary>
+        protected AppDbContext context;
+        /// <summary>
+        /// ロガー
+        /// </summary>
+        protected readonly ILogger logger;
+
+        /// <summary>
+        /// コンストラクタ。
+        /// </summary>
+        /// <param name="context">DBコンテキスト</param>
+        /// <param name="logger">ロガー</param>
+        public RepositoryBase(AppDbContext context, ILogger logger)
+        {
+            this.context = context;
+            this.logger = logger;
+        }
+
+        /// <summary>
+        /// エンティティを登録します。
+        /// </summary>
+        /// <param name="entity">登録エンティティ</param>
+        public void InsEntity(EntityBase entity)
+        {
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        /// <summary>
+        /// エンティティを更新します。
+        /// </summary>
+        /// <param name="entity">更新エンティティ</param>
+        public void UpdEntity(EntityBase entity)
+        {
+            context.Attach(entity);
+            context.SaveChanges();
+        }
+    }
+}
